fa8874a0f4a68e2faec0ceb11fd3573e4ab3678e
PlaryWasTaken <gabrielcararo@gmail.com>
2023-03-05 22:55:09 -0300

    Changed e-mail verifier to verify MX aswell and fixed some bugs

--- index.ts
+++ index.ts
@@ -5,17 +5,26 @@ import {PSTFile, PSTFolder} from "pst-extractor";
 import * as cliProgress from 'cli-progress';
 import chalk from 'chalk';
 import {terminal} from "terminal-kit";
-import * as os from "os";
 
+import * as os from "os";
+let threadCount = os.availableParallelism()
 let domains = new discord.Collection<string, string[]>();
 const activeWorkers: Worker[] = [];
 const startingData = {domains: domains, path: '', filePath: 'F:\\apps\\a.pst'}
 const queue: any[] = [];
 const finished: any[] = [];
 
-const file = new PSTFile(startingData.filePath);
-const mainFolder = file.getRootFolder();
+const Chalk = new chalk.Instance({level: 3});
+const multiBar = new cliProgress.MultiBar({
+    barsize: 80,
+    format: '{name} | ' + chalk.hex('#ffffff')('{bar}') + ` | ${chalk.hex('#aa00ff')('{percentage}%')} | {value}/{total}`,
+    forceRedraw: false,
+    hideCursor: true,
+    fps: 20
+})
 
+const paths: string[] = [];
+const pathsCompleted = new Set();
 
 function recurseThroughFolders(folder: PSTFolder, path: string): string[] {
     const paths = [];
@@ -29,22 +38,10 @@ function recurseThroughFolders(folder: PSTFolder, path: string): string[] {
     }
     return paths;
 }
-
-const Chalk = new chalk.Instance({level: 3});
-let allPaths = recurseThroughFolders(mainFolder, '')
-const multiBar = new cliProgress.MultiBar({
-    barsize: 80,
-    format: '{name} | ' + chalk.hex('#ffffff')('{bar}') + ` | ${chalk.hex('#aa00ff')('{percentage}%')}`,
-    forceRedraw: true,
-    hideCursor: true,
-    fps: 20
-})
-
-const paths: string[] = [];
-const pathsCompleted = new Set();
-let threadCount = os.availableParallelism()
-
-function getSubfolders(path: string, filePath: string, mainProgress: cliProgress.SingleBar, bars: Map<string, cliProgress.SingleBar>) {
+const validEmails:string[] = []
+const invalidEmails:string[] = []
+let totalEmailsChecked = 0
+function getSubfolders(path: string, filePath: string, mainProgress: cliProgress.SingleBar, bars: Map<string, cliProgress.SingleBar>, allPaths: string[]) {
     const worker = new Worker('./worker.js', {workerData: {domains: domains, path: path, filePath: filePath}});
     activeWorkers.push(worker);
     worker.on('message', (data) => {
@@ -52,7 +49,7 @@ function getSubfolders(path: string, filePath: string, mainProgress: cliProgress
             // console.log(data.msg);
         }
         if (data.createBar) {
-            console.clear()
+            totalEmailsChecked += data.contentCount
             bars.set(data.folderName, multiBar.create(data.contentCount, 0, {name: data.folderName}))
         }
         if (data.increment) {
@@ -64,18 +61,23 @@ function getSubfolders(path: string, filePath: string, mainProgress: cliProgress
                 allPaths = paths;
             }
             queue.push(data);
-            terminal.clear();
         }
         if (data.finished) {
+            console.clear()
             pathsCompleted.add(data.currentPath)
             const a = pathsCompleted.size / allPaths.length * 100
-            mainProgress.update(a)
+            mainProgress.update(Math.floor(a))
             const b = bars.get(data.folderName)
             if (b) {
                 b.stop();
                 multiBar.remove(b);
             }
             activeWorkers.splice(activeWorkers.indexOf(worker), 1);
+
+            validEmails.push(...data.validEmails)
+            invalidEmails.push(...data.invalidEmails)
+
+
             for (const domain of data.domains) {
                 if (domains.has(domain[0])) {
                     domains.get(domain[0])?.push(...domain[1]);
@@ -87,25 +89,23 @@ function getSubfolders(path: string, filePath: string, mainProgress: cliProgress
                 mainProgress.stop()
                 const domainNames = Array.from(domains.keys());
                 fs.writeFileSync('domains.txt', domainNames.join('\n'));
-                const emails = new Set();
-                for (const domain of domains) {
-                    for (const email of domain[1]) {
-                        emails.add(email);
-                    }
-                }
-                fs.writeFileSync('emails.txt', Array.from(emails).join('\n'));
-                process.exit(0);
+
+                fs.writeFileSync('emails.txt', validEmails.join('\n'));
+                terminal.clear();
+                ended = true;
+                console.log(chalk.green(`Processo finalizado!`) +
+                `\n${chalk.blue(`Foram checados um total de ${totalEmailsChecked} emails e ${validEmails.length + invalidEmails.length} enderecos de email.`)}\nForam encontrados ${validEmails.length} emails ${chalk.green('válidos')} e ${invalidEmails.length} enderecos de emails unicos ${chalk.red('inválidos')} foram encontrados.\nAperte qualquer tecla para sair`)
             }
             if (queue.length > 0) {
                 const next = queue.shift();
                 finished.push(next);
                 //console.log(next.childPath)
-                getSubfolders(next.childPath, filePath, mainProgress, bars);
+                getSubfolders(next.childPath, filePath, mainProgress, bars, allPaths);
                 while (activeWorkers.length <= threadCount && queue.length > 0) {
                     const next = queue.shift();
                     finished.push(next);
                     //console.log(next.childPath)
-                    getSubfolders(next.childPath, filePath, mainProgress, bars);
+                    getSubfolders(next.childPath, filePath, mainProgress, bars, allPaths);
                 }
 
             }
@@ -115,7 +115,8 @@ function getSubfolders(path: string, filePath: string, mainProgress: cliProgress
     })
 }
 
-terminal('Escola um arquivo PST: ');
+let ended = false;
+terminal('Escolha um arquivo PST: ');
 terminal.fileInput({baseDir: '../'}, (error, path) => {
     if (error) {
         console.error(error);
@@ -132,13 +133,21 @@ terminal.fileInput({baseDir: '../'}, (error, path) => {
         terminal.grabInput(true);
         console.log(chalk.green(`Pressione ENTER para iniciar o processo de extração de emails e domínios com ${threadCount} threads. Ctrl+C para cancelar. Ctrl + Q para modificar a quantidade de threads.`))
         terminal.on('key', (name: string, matches: any, data: any) => {
-            if (name === 'ENTER') {
+            if (ended) {
+                process.exit(0);
+            }
+            if (name === 'ENTER' && !ended) {
+                terminal.grabInput(false);
+                const file = new PSTFile(path);
+                const mainFolder = file.getRootFolder();
+                const allPaths = recurseThroughFolders(mainFolder, '')
                 const mainProgress = multiBar.create(100, 0, {
                     name: Chalk.hex('#ffaaff')('Main Progress'),
                     color: chalk.blue('')
                 })
                 const bars = new Map<string, cliProgress.SingleBar>();
-                getSubfolders(startingData.path, path, mainProgress, bars);
+                getSubfolders(startingData.path, path, mainProgress, bars, allPaths);
+                terminal.hideCursor(false);
             }
             if (name === 'CTRL_C') {
                 terminal.grabInput(false);
@@ -165,6 +174,5 @@ terminal.fileInput({baseDir: '../'}, (error, path) => {
                 })
             }
         })
-
     }
 })
--- worker.ts
+++ worker.ts
@@ -1,28 +1,37 @@
-import {PSTFolder, PSTFile} from "pst-extractor";
-import {parentPort, workerData, isMainThread} from 'worker_threads';
+import {PSTFile, PSTFolder} from "pst-extractor";
+import {isMainThread, parentPort, workerData} from 'worker_threads';
 import {resolve} from "path";
-import { Collection } from "discord.js";
-import * as emailChecker from "email-addresses";
+import {Collection} from "discord.js";
+import {verifyEmail} from '@devmehq/email-validator-js';
+
 const file = new PSTFile(resolve(workerData.filePath));
 const mainFolder = file.getRootFolder();
-function validateEmail(email:string) {
-    return emailChecker.parseOneAddress(email) !== null;
+
+async function validateEmail(email: string) {
+    const {validFormat, validMx} = await verifyEmail({
+        verifyMx: true,
+        verifySmtp: false,
+        timeout: 3000,
+        emailAddress: email
+    });
+    return validFormat && validMx;
 }
 
 function findCurrentFolder(path: string) {
-    parentPort?.postMessage({ pathReceived: path })
+    parentPort?.postMessage({pathReceived: path})
     const pathArray = path.split('.');
     let currentFolder = mainFolder;
-    parentPort?.postMessage({ currentPath: '' })
+    parentPort?.postMessage({currentPath: ''})
     for (let i = 1; i < pathArray.length; i++) {
-        parentPort?.postMessage({ currentPathLoop: pathArray[i] })
+        parentPort?.postMessage({currentPathLoop: pathArray[i]})
         const folders = currentFolder.getSubFolders();
-        const folder = folders.find((folder:PSTFolder) => folder.displayName === pathArray[i]);
+        const folder = folders.find((folder: PSTFolder) => folder.displayName === pathArray[i]);
         if (!folder) throw new Error("Folder not found");
         currentFolder = folder;
     }
     return currentFolder;
 }
+
 if (isMainThread) {
     throw new Error("This file is not meant to be run in the main thread.");
 } else {
@@ -30,39 +39,61 @@ if (isMainThread) {
         if (!parentPort) throw new Error("This file is not meant to be run in the main thread.");
         const domains = new Collection<string, string[]>();
         const emailsUsed = new Set();
+        const validEmails = new Set();
+        const invalidEmails = new Set();
         const folder = findCurrentFolder(workerData.path)
         if (folder.hasSubfolders) {
-            parentPort.postMessage({ folderName: folder.displayName, hasSubfolders: true })
+            parentPort.postMessage({folderName: folder.displayName, hasSubfolders: true})
             let childFolders = folder.getSubFolders();
-            parentPort.postMessage({ childFolders: childFolders.length })
+            parentPort.postMessage({childFolders: childFolders.length})
             for (let childFolder of childFolders) {
-                parentPort.postMessage({ summonWorker: true, folder: folder.displayName, currentPath: workerData.path, childFolder: childFolder.displayName, childPath: workerData.path + '.' + childFolder.displayName, domains: domains  })
+                parentPort.postMessage({
+                    summonWorker: true,
+                    folder: folder.displayName,
+                    currentPath: workerData.path,
+                    childFolder: childFolder.displayName,
+                    childPath: workerData.path + '.' + childFolder.displayName,
+                    domains: domains
+                })
             }
         }
         if (folder.contentCount > 0) {
-            parentPort.postMessage({ createBar: true, folderName: folder.displayName, contentCount: folder.contentCount })
+            parentPort.postMessage({createBar: true, folderName: folder.displayName, contentCount: folder.contentCount})
             let email = folder.getNextChild();
             while (email != null) {
-                parentPort.postMessage({ increment: true, folderName: folder.displayName })
+                parentPort.postMessage({increment: true, folderName: folder.displayName})
                 const from = email.senderEmailAddress;
-                emailsUsed.add(from);
-                if (!emailsUsed.has(from)) {
-                    const a = validateEmail(from)
-                    if (from !== '') parentPort.postMessage({ msg: `Validating ${from}... ${a?'Valid':'Invalid'}` })
-                    if (!a) continue;
 
+                if (!emailsUsed.has(from)) {
+                    emailsUsed.add(from);
+                    const a = await validateEmail(from)
+                    if (from !== '') parentPort.postMessage({msg: `Validating ${from}... ${a ? 'Valid' : 'Invalid'}`})
+                    if (!a) {
+                        invalidEmails.add(from);
+                        continue
+                    }
+                    validEmails.add(from);
                     const domain = from.split('@')[1];
                     if (domains.has(domain)) {
                         domains.get(domain)?.push(from);
                     } else {
                         domains.set(domain, [from]);
                     }
-
                 }
                 email = folder.getNextChild();
             }
         }
-        parentPort.postMessage({ finished: true, domains: domains, folderName: folder.displayName, currentPath: workerData.path, contentCount: folder.contentCount })
+        parentPort.postMessage({
+            finished: true,
+            domains: domains,
+            folderName: folder.displayName,
+            currentPath: workerData.path,
+            contentCount: folder.contentCount,
+            validEmails: Array.from(validEmails),
+            invalidEmails: Array.from(invalidEmails)
+        })
     }
-    main().then(() => {});
+
+    main().then(() => {
+    });
 }
